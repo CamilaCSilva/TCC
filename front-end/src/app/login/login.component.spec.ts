86161cb8cd4a093f1dd86d2f8565f6d2b7714d57
Camila Silva <camila.silva@watchguard.com>
2023-03-28 19:13:58 -0300

    Página de login aplicação médica

---
+++ front-end/src/app/login/login.component.spec.ts
@@ -0,0 +1,38 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginComponent } from './login.component';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  fdescribe('entrar()', () => {
+    it('should show message', () => {
+      // Given
+      const name = 'Camila';
+
+      // When
+      component.entrar(name);
+
+      // Then
+      expect(component.mensagem).toEqual("Bem vindo(a), " + name + "!");
+    });
+  });
+});
