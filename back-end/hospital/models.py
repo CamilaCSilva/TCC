47849d81872acb84b017d64a288852d56abbe5ae
isadorabello <isadora.bello@gec.inatel.br>
2023-04-26 19:33:20 -0300

    adding validation (not working)

--- back-end/hospital/models.py
+++ back-end/hospital/models.py
@@ -15,4 +15,5 @@ class ProfissionaldeSaude(models.Model):
     def __str__(self):
         return self.nomeCompleto
     
-    
+class BaixaFicha(models.Model):
+    DOCUMENTOS = ( ('', ''), ('', ''), ('', ''))
