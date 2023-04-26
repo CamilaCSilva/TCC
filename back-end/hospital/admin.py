902164397d40c014fd2e416e5b932c5fa6ddd17f
isadorabello <isadora.bello@gec.inatel.br>
2023-04-25 21:52:52 -0300

    adding validation

--- back-end/hospital/admin.py
+++ back-end/hospital/admin.py
@@ -4,8 +4,8 @@ from hospital.models import ProfissionaldeSaude
 # Register your models here.
 
 class ProfissionaisdeSaude(admin.ModelAdmin):
-    list_display = ( 'cpf', 'nomeCompleto')
+    list_display = ( 'cpf', 'nomeCompleto', 'campo_escolha' ,'documentoTrabalho')
     list_display_links = ('cpf',)
-    search_fields = ( 'cpf', )
+    search_fields = ( 'cpf', 'nomeCompleto' )
 
 admin.site.register(ProfissionaldeSaude, ProfissionaisdeSaude)
--- back-end/hospital/serializer.py
+++ back-end/hospital/serializer.py
@@ -1,19 +1,16 @@
 from rest_framework import serializers
 from hospital.models import ProfissionaldeSaude
+from hospital.validators import *
+
 
 class ProfissionaldeSaudeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProfissionaldeSaude
         fields = '__all__'
-
-        """
-        def validate(self, data):
-            if not nome_valido(data['nome']):
-                raise serializers.ValidationError({'nome':"Não inclua números neste campo"})
-            if not cpf_valido(data['cpf']):
-                raise serializers.ValidationError({'cpf':"O CPF deve ter 11 dígitos"})
-            if not telefone_valido(data['telefone']):
-                raise serializers.ValidationError({'telefone':"O telefone deve informar o DDD e o 9 padrão"})
-            
-            return data
-        """
+    
+    def validate(self, data):
+        if not nome_valido(data['nomeCompleto']):
+            raise serializers.ValidationError({'nomeCompleto':"Não inclua números neste campo"})
+        return data
+    
+        
