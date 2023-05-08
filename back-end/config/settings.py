1b10d436c25c629eef87477a041d7023c6eae89e
isadorabello <isadora.bello@gec.inatel.br>
2023-05-08 20:34:27 -0300

    feat: adjusting some front and back issues

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'hospital',
     'ficha_paciente',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -127,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
--- front-end/src/app/perfil/perfil.component.ts
+++ front-end/src/app/perfil/perfil.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-perfil',
@@ -22,9 +23,15 @@ export class PerfilComponent implements OnInit {
     celular: '(35)99123-4567'
   }
 
-  constructor(private router: Router) { }
+  obj:any;
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.obj = this.http.get("http://127.0.0.1:8000/profissionaisdesaude/12345678910/").subscribe(
+      data => this.obj = data
+    )
+
   }
 
   editar(){
