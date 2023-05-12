c0607e83eb36ee9c38e7967cc77b617edd647539
Matheus Chagas da Silva <85804680+Matheusilva431@users.noreply.github.com>
2023-05-12 00:13:06 -0300

    Update editar-perfil.component.ts

--- front-end/src/app/editar-perfil/editar-perfil.component.ts
+++ front-end/src/app/editar-perfil/editar-perfil.component.ts
@@ -68,22 +68,21 @@ export class EditarPerfilComponent implements OnInit {
     return this.areaAtuacao;
   }
 
-  // private verificaDados() {
-  //   if(this.nomeCompleto && this.nomeCompleto.length < 6 && this.nomeCompleto.match(/([A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+)|([a-záàâãéèêíïóôõöúçñ ]+)/) == null) {
-  //     alert('Nome incompleto');
-  //     throw new Error('Nome incompleto');
-  //   }
-  //   else if (this.cpf && this.cpf.length < 11 || !this.cpf.match(new RegExp('^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}$'))) {
-  //     alert('CPF incompleto');
-  //     throw new Error('CPF incompleto');
-  //   }
-  //   else if (this.celular && this.celular.match(/(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))/) == null){
-  //     alert('Celular no formato inesperado');
-  //     throw new Error('Celular incorreto');
-  //   }
-  //   else {
-  //     this.testResult = true;
-  //   }
-  // }
-
+  private verificaDados() {
+    if(this.nomeCompleto && this.nomeCompleto.length < 6) {
+      alert('Nome incompleto');
+      throw new Error('Nome incompleto');
+    }
+    else if (this.cpf && this.cpf.length < 11 || !this.cpf.match(new RegExp('^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}$'))) {
+      alert('CPF incompleto');
+      throw new Error('CPF incompleto');
+    }
+    else if (this.celular && this.celular.match(/(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))/) == null){
+      alert('Celular no formato inesperado');
+      throw new Error('Celular incorreto');
+    }
+    else {
+      this.testResult = true;
+    }
+  }
 }
