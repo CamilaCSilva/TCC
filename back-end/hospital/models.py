f78a9bef18f157eec680058b92300b1ccc958d4d
isadorabello <isadora.bello@gec.inatel.br>
2023-05-22 15:01:36 -0300

    ajustando criação superuser

--- back-end/hospital/models.py
+++ back-end/hospital/models.py
@@ -1,18 +1,25 @@
 from django.contrib.auth.models import AbstractUser
+from config import settings
 from django.db import models
 from ficha_paciente.models import Anamnese
 
-# Create your models here.
+
+class User(AbstractUser):
+    cpf = models.CharField(max_length=11, primary_key=True, unique=True)
+
+    USERNAME_FIELD = "cpf"
+    REQUIRED_FIELDS = ["username"]
+
 class ProfissionaldeSaude(models.Model):
     DOCUMENTOS = ( ('CRM', 'Médico(a)'), ('COREN', 'Enfermeiro(a)'), ('DRF', 'Paramédico(a)'))
 
-    cpf = models.DecimalField(max_digits=11, decimal_places=0, primary_key=True)
+    cpf = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     campo_escolha = models.CharField(max_length=5, choices=DOCUMENTOS, blank=False, null=False, default='CRM')
     nome_completo = models.CharField(max_length=150)
     celular = models.DecimalField(max_digits=11, decimal_places=0)
     documento_trabalho = models.CharField(max_length=10)
     unidade_de_atendimento = models.CharField(max_length=250)
-    senha = models.CharField(max_length=128)
+    senha = models.CharField(User, max_length=128)
 
     def __str__(self):
         return self.nome_completo
@@ -20,11 +27,5 @@ class ProfissionaldeSaude(models.Model):
 
 class FichaPaciente(models.Model):
     anamnese = models.ForeignKey(Anamnese, on_delete=models.CASCADE, default='0')
-    paramedico = models.ForeignKey(ProfissionaldeSaude, on_delete=models.CASCADE, default='0')
+    paramedico = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-
-class User(AbstractUser):
-    cpf = models.DecimalField(max_digits=11, decimal_places=0, unique=True)
-
-    USERNAME_FIELD = "cpf"
-    REQUIRED_FIELDS = ["username"]
