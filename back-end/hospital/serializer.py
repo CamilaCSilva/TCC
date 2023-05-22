8cd8e469c8ee51380e2ba893813e9dc131ee9b5d
isadorabello <isadora.bello@gec.inatel.br>
2023-05-22 11:56:36 -0300

    ajustando detalhes

--- back-end/hospital/serializer.py
+++ back-end/hospital/serializer.py
@@ -1,21 +1,10 @@
 from rest_framework import serializers
 from hospital.models import ProfissionaldeSaude, FichaPaciente
-from hospital.validators import *
-
 
 class ProfissionaldeSaudeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProfissionaldeSaude
         fields = '__all__'
-    
-    # def validate(self, data):
-    #     if not cpf_valido(data['cpf']):
-    #         raise serializers.ValidationError({'cpf':"O CPF deve ter 11 dígitos"})
-    #     if not nome_valido(data['nome_completo']):
-    #         raise serializers.ValidationError({'nome_completo':"Não inclua números neste campo"})
-    #     if not celular_valido(data['celular']):
-    #         raise serializers.ValidationError({'celular':"O número de telefone deve conter o DDD e o 9 padrão"})
-    #     return 
 
 class FichasPacientesSerializer(serializers.ModelSerializer):
     class Meta:
