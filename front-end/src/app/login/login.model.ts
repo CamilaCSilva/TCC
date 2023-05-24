2a3a14936869b7ffbe050c97cf7a3c5e5ae3bbd8
Matheus Chagas da Silva <85804680+Matheusilva431@users.noreply.github.com>
2023-05-24 19:36:26 -0300

    Delete login.model.ts

--- front-end/src/app/login/login.model.ts
+++
@@ -1,8 +0,0 @@
-// VOU PEGAR INFOS DO BANCO
-export interface LoginInfoExistente {
-  cpf: string;
-  password: string;
-  is_correct: boolean;
-}
-
-export interface LoginInfoExistenteArray extends Array<LoginInfoExistente> {}
