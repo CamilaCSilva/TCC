883c68942d65d1684bef718c6e015b94dbc47039
Matheus Chagas da Silva <85804680+Matheusilva431@users.noreply.github.com>
2023-05-25 19:30:35 -0300

    Update perfil.service.ts

--- front-end/src/app/perfil/perfil.service.ts
+++ front-end/src/app/perfil/perfil.service.ts
@@ -2,14 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PerfilInfo } from '../models/perfil.model';
 import { Observable } from 'rxjs';
-import { URLS } from '../models/url.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PerfilService {
 
-  url = URLS;
+  <!--   url = 'https://tcc-production-33a0.up.railway.app/'; -->
+  url = 'http://localhost:8000/'
+
+  private profissional_url = url
 
   private profissionalUrl = this.url + 'profissionaldesaude/?cpf='
 
