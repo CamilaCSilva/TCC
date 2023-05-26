8f1eafb4a2ec35b20a92188f91e3aeb2c4181ee3
Matheusilva431 <matheusilva431+github@gmail.com>
2023-05-26 02:00:10 -0300

    Ajuste nas URLs

--- front-end/src/app/perfil/perfil.service.ts
+++ front-end/src/app/perfil/perfil.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PerfilInfo } from '../models/perfil.model';
 import { Observable } from 'rxjs';
@@ -11,11 +11,17 @@ export class PerfilService {
   private url = 'https://tcc-production-33a0.up.railway.app/';
   // private url = 'http://localhost:8000/'
 
+  header: any = localStorage.getItem('Token')
+
   private profissional_url = this.url + 'profissionaldesaude/?cpf=13168035629' // trocar o ?cpf=13168035629 por user
 
   constructor(private http: HttpClient) {}
 
   getPerfilInfo(cpf: any) {
-    return this.http.get(`${this.profissional_url}`) as Observable<PerfilInfo>;
+    return this.http.get(      
+      `${this.profissional_url}`,{
+        headers: this.header,
+      }
+      ) as Observable<PerfilInfo>;
   }
 }
