1398778acce63eb2e9b06af04db2b66d042b0026
isadorabello <isadora.bello@gec.inatel.br>
2023-11-06 15:53:16 -0300

    clean: retirando codigos comentados

--- back-end/hospital/admin.py
+++ back-end/hospital/admin.py
@@ -3,14 +3,6 @@ from hospital.models import ProfissionaldeSaude
 
 # Register your models here.
 
-"""
-class Users(admin.ModelAdmin):
-    list_display = ( 'cpf',)
-    list_display_links = ('cpf',)
-    search_fields = ( 'cpf',)
-
-admin.site.register(User, Users)
-"""
 
 class ProfissionaisdeSaude(admin.ModelAdmin):
     list_display = ( 'cpf', 'nome_completo', 'campo_escolha' ,'documento_trabalho')
@@ -18,11 +10,3 @@ class ProfissionaisdeSaude(admin.ModelAdmin):
     search_fields = ( 'cpf', 'nome_completo' )
 
 admin.site.register(ProfissionaldeSaude, ProfissionaisdeSaude)
-
-"""
-class ResponsavelAnamnese(admin.ModelAdmin):
-    list_display = ( 'id','anamnese', 'paramedico')
-    list_display_links = ('id',)
-
-admin.site.register(FichaPaciente, ResponsavelAnamnese)
-"""
